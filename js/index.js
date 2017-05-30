49700d82355d6f88060b6e02c827980c5fab3ac1
Luciano Ramello <lucianoramello@MacBook-Pro-de-Luciano.local>
2017-05-30 15:36:00 -0300

    agregado mas funcionalidad a los botones y a los links, agregado de script para un scroll mas suave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log("Funciona");
+(function(){
+  console.log("funciona");
+  $( document ).ready(function() {
+      console.log( "ready!" );
+      $(document).on('click', 'a', function(event){
+          event.preventDefault();
+
+          $('html, body').animate({
+              scrollTop: $( $.attr(this, 'href') ).offset().top
+          }, 550);
+      });
+  });
+})();
+
+  // $(document).ready(function(){
+  //   console.log('entra en el click');
+  //   $('#divID').getNiceScroll().doScrollPos(0,('#about').position().top);
+  // });
